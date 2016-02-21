bfae17c00bc2575de5002d78e0785fd63576c707
Carlos Mendez <calicci.gardachi@gmail.com>
2016-02-20 19:19:40 -0500

    Adding all the preconfigured files to the project
    This set of files will get the user to a fully configured project with FOSUserBundle installed according to recommended best practices and has bundle inheritance configured which means you can override any part of the FOSUserBundle templates or controllers safely without risk of breaking the functionality.

---
+++ UserBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hive
+ * Date: 2/19/16
+ * Time: 8:03 AM
+ */
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
